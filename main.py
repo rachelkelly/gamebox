c8d9cc8dd218fc2022669746df4e62e8956807ab
rachelkelly <rachelkelly@gmail.com>
2013-04-27 12:49:05 -0700

    ..slightly better?  feh

--- main.py
+++ main.py
@@ -1,42 +1,41 @@
+##copyright 2013 rachel kelly
+##fightysandbox
+
 import random
-global hero = input("what is your mighty and powerful call? ")
-print("greetings,",hero,".")
-print("\nYou encounter a",monster,"!")
-
-#### heyo!  all these monster defs, I don't think the internal HP
-#### values need to have different variables, 'cos they're not globals!
-
-def monsterOne():
-	monsterHP = 8
-
-def monsterTwo():
-	monsterHP = 13
-
-def monsterThree():
-	monsterHP = 21
-
-def monsterFour():
-	monsterHP = 34
-
-def monsterFive():
-	monsterHP = 55
+## necessary this guy below?  probably eventually.
+from monsterlist.py import *
 
 def main():
-	monster = random.randint(0,4)
-	if monster == 0:
-		monsterLevelOne()
-	elif monster == 1:
-		monsterLevelTwo()
-	elif monster == 2:
-		monsterLevelThree()
+	global hero = input("what is your mighty and powerful call? ")
+	print("greetings,",hero,".")
+	print("\nYou encounter a",monster,"!")
+	##need to define monster var somewhere
+	if heroLevel > 40:
+		monsterChoice = random.choice(monsters_level_four)
+	elif heroLevel > 30:
+		monsterChoice = random.choice(monsters_level_three)
+	elif heroLevel > 20:
+		monsterChoice = random.choice(monsters_level_two)
+	elif heroLevel > 10:
+		monsterChoice = random.choice(monsters_level_one)
+	elif heroLevel >= 1:
+		monsterChoice = random.choice(monsters_level_oh)
+	else:
+		print("ERROR")
+	while currentHP >= 0:
+		battleMenu()
 
+def battleMenu():
+	menuChoice = input("do you want to hit via (m)elee, cast a (s)pell, or use an (i)tem?")
+	if menuChoice == m:
+		melee()
+	elif menuChoice == s:
+		magic()
+	elif menuChoice == i:
+		items()
+	else:
+		battleMenu()
 
-def monsterLevelOne():
-	monsterHP = random.randrange(13,21)
-	monsterHP
-
-def monsterLevelTwo():
-	monsterHP = random.randrange(21,34)
 
 def magic(monster):
 	spellDict = {"fire","ice","lightning"}
@@ -44,24 +43,13 @@ def magic(monster):
 	if spell in spellDict:
 		##if spell == "fire"? can I do this?
 		##not sure if I even need a dict here.  could just be a bunch of if/elifs?
-		print("you cast",spell,"on",monster".")
+		currentHP = monsterHP
+		while currentHP >= 0:
+			print("you cast",spell,"on",monster".")
 
 def items():
 	itemDict = dict()
 	file = open("items.txt","r")
-
-
-def battleMenu():
-	battleChoice = input("do you want to (h)it with your sword, cast (m)agic, or use an (i)tem? ")
-	if battleChoice == "h":
-		melee(monster)
-	elif battleChoice == "m":
-		magic(monster)
-	elif battleChoice == "i":
-		items()
-	else:
-		print("try again",hero,".")
-		battleMenu()
 
 def hit(monsterHP):
 	print("your sword connects!")
