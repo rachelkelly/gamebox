e27a20d949f7ec6f02e11053d4a0870a665a8089
rachelkelly <rachelkelly@gmail.com>
2013-04-27 12:49:39 -0700

    Create monsterlist.py

---
+++ monsterlist.py
@@ -0,0 +1,32 @@
+def monsters_level_one():
+  def monsterOh():
+		monsterHP = 5
+	def monsterOne():
+		monsterHP = 8
+	def monsterTwo():
+		monsterHP = 13
+	def monsterThree():
+		monsterHP = 21
+
+def monsters_level_two():
+	def monsterFour():
+		monsterHP = 34
+	def monsterFive():
+		monsterHP = 55
+
+def monsters_level_one():
+	monsters_level_one = []
+	monsters_level_one.append(monster)
+
+def main():
+	monster = random.randint(0,4)
+	if monster == 0:
+		monsters_level_oh = [grue, baby badger, tiny butt]
+	elif monster == 1:
+		monsters_level_one = [squirrel, kind orc, bag o dicks]
+	elif monster == 2:
+		monsters_level_two = [talking yurt, bear, bad wombat]
+	elif monster == 3:
+		monsters_level_three = [yeti, menacing vuvuzuela, orthoblap]
+	elif monster == 4:
+		monsters_level_four = [sbvd, big boss, captain pantalon]
