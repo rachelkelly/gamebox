b32528d3701c0b75c8f443fc7ad2a5f22ff84db3
rachelkelly <rachelkelly@gmail.com>
2013-04-27 13:00:03 -0700

    this's way better.
    just have to make sure I'm not really duplicating anything

--- monsterlist.py
+++ monsterlist.py
@@ -1,32 +1,22 @@
-def monsters_level_one():
-  def monsterOh():
-		monsterHP = 5
-	def monsterOne():
-		monsterHP = 8
-	def monsterTwo():
-		monsterHP = 13
-	def monsterThree():
-		monsterHP = 21
-
-def monsters_level_two():
-	def monsterFour():
-		monsterHP = 34
-	def monsterFive():
-		monsterHP = 55
-
-def monsters_level_one():
-	monsters_level_one = []
-	monsters_level_one.append(monster)
-
-def main():
-	monster = random.randint(0,4)
-	if monster == 0:
+def monster_type():
+	monsterChooser = random.randint(0,4)
+	if monsterChooser == 0:
 		monsters_level_oh = [grue, baby badger, tiny butt]
-	elif monster == 1:
+		monster = random.choice(monsters_level_oh)
+		print(monster)
+	elif monsterChooser == 1:
 		monsters_level_one = [squirrel, kind orc, bag o dicks]
-	elif monster == 2:
+		monster = random.choice(monsters_level_one)
+		print(monster)
+	elif monsterChooser == 2:
 		monsters_level_two = [talking yurt, bear, bad wombat]
-	elif monster == 3:
+		monster = random.choice(monsters_level_two)
+		print(monster)
+	elif monsterChooser == 3:
 		monsters_level_three = [yeti, menacing vuvuzuela, orthoblap]
-	elif monster == 4:
+		monster = random.choice(monsters_level_three)
+		print(monster)
+	elif monsterChooser == 4:
 		monsters_level_four = [sbvd, big boss, captain pantalon]
+		monster = random.choice(monsters_level_four)
+		print(monster)
