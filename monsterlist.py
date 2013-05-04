2fa8f22a33cbe36cb055afca55ea4d7adde93d59
rachelkelly <rachelkelly@gmail.com>
2013-05-04 12:52:51 -0700

    gotta define each monster

--- monsterlist.py
+++ monsterlist.py
@@ -1,22 +1,26 @@
+##autopopulate with hp etc?
+##naaaah
+
+def grue():
+	monsterHP = 4
+	monsterMP = 0
+	melee1 = print("the grue hugs you, really meanly")
+
+def babybadger():
+	monsterHP = 5
+	monsterMP = 0
+	melee1 = print("the baby badger scratches you while you snuggle him.  ow, damnit!")
+
+def tinybutt():
+	monsterHP = 6
+	monsterMP = 100
+	melee1 = print("guess what\n\n\n\n\nfaaaaaaaaaart")
+
+
+
 def monster_type():
-	monsterChooser = random.randint(0,4)
-	if monsterChooser == 0:
-		monsters_level_oh = [grue, baby badger, tiny butt]
-		monster = random.choice(monsters_level_oh)
-		print(monster)
-	elif monsterChooser == 1:
-		monsters_level_one = [squirrel, kind orc, bag o dicks]
-		monster = random.choice(monsters_level_one)
-		print(monster)
-	elif monsterChooser == 2:
-		monsters_level_two = [talking yurt, bear, bad wombat]
-		monster = random.choice(monsters_level_two)
-		print(monster)
-	elif monsterChooser == 3:
-		monsters_level_three = [yeti, menacing vuvuzuela, orthoblap]
-		monster = random.choice(monsters_level_three)
-		print(monster)
-	elif monsterChooser == 4:
-		monsters_level_four = [sbvd, big boss, captain pantalon]
-		monster = random.choice(monsters_level_four)
-		print(monster)
+	monsters_level_oh = [grue, babybadger, tinybutt]
+	monsters_level_one = [squirrel, kindorc, bagodicks]
+	monsters_level_two = [talkingyurt, bear, badwombat]
+	monsters_level_three = [yeti, menacingvuvuzuela, orthoblap]
+	monsters_level_four = [sbvd, bigboss, captainpantalon]
